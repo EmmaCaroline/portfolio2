0ab29435d0b7cee5680ad0e3573f0b46d458480d
EmmaCaroline <emma-caroline@hotmail.com>
2025-06-08 04:45:29 +0200

    update About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,15 +18,33 @@ const About = () => {
             className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-40 lg:h-40 rounded-full object-cover self-start"
             src={ProfileImage}
           />
-          <p className="text-white font-body text-sm md:text-base sm:w-1/2 mt-6 sm:mt-0">
-            about text here about text here about text here about text here
-            about text here about text here about text here about text here
-            about text here about text here about text here about text here
-            about text here about text here about text here about text here
-            about text here about text here about text here about text here
-            about text here about text here about text here about text here
-            about text here about text here
-          </p>
+          <div className="sm:w-1/2 mt-6 sm:mt-0">
+            <p className="text-gray-300 font-body text-sm">
+              My name is Emma Caroline Brander, and I'm a front-end developer
+              based in Vestfold, Norway. I'm currently in the final stage of my
+              studies, will soon graduate with a Higher Professional Degree in
+              Frontend Development.
+            </p>
+            <p className="text-gray-300 font-body text-sm mt-4">
+              I started my coding journey in 2022, and was immediately drawn to
+              the creative and logical aspects of web development. That spark
+              has shaped my journey through hands-on projects and collaborative
+              work, inspiring me to explore modern tools and frameworks and to
+              write clean, maintainable code.
+            </p>
+            <p className="text-gray-300 font-body text-sm mt-4">
+              I'm currently sharpening my frontend skills and expanding into
+              backend development with Python (Flask/Django), Node.js (Express),
+              and PostgreSQL, building on my existing experience with MySQL —
+              aiming to become a fullstack developer.
+            </p>
+            <p className="text-gray-300 font-body text-sm mt-4">
+              Beyond my interest in programming, I enjoy being in nature —
+              hiking or wild camping — and I also enjoy gaming with friends. I
+              enjoy every opportunity to learn something new, and work hard to
+              achieve the goals I set for myself.
+            </p>
+          </div>
         </div>
         <p className="text-white font-body text-sm md:text-base mt-8 sm:ml-[72px] md:ml-[90px] lg:ml-[126px] flex items-center gap-6 sm:gap-3 mb-6">
           Contact:
