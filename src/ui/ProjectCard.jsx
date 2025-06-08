b9b031d68e553d51e1f3b517245e4ecbf7c0ed6d
EmmaCaroline <emma-caroline@hotmail.com>
2025-06-08 04:47:43 +0200

    update font ProjectCard

--- src/ui/ProjectCard.jsx
+++ src/ui/ProjectCard.jsx
@@ -5,7 +5,7 @@ const ProjectCard = ({ project }) => {
     <Link to={`/project/${project.id}`}>
       <div className="group">
         <div className="h-full w-full rounded-lg border border-primary py-2 px-4 bg-black transition duration-300 ease-linear group-hover:scale-105">
-          <h2 className="text-white pb-4 text-base md:text-lg">
+          <h2 className="text-white pb-4 font-heading text-base md:text-lg">
             {project.title}
           </h2>
           <div className="block w-full h-56 overflow-hidden relative pb-2">
@@ -15,7 +15,9 @@ const ProjectCard = ({ project }) => {
               className="w-full h-full object-cover"
             />
           </div>
-          <p className="text-gray-300">{project.descriptionShort}</p>
+          <p className="text-gray-300 font-body text-sm">
+            {project.descriptionShort}
+          </p>
         </div>
       </div>
     </Link>
